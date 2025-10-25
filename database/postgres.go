fe0dd02250b4c6ee05e63d4cffa7085f6f1e58d6
therealagt <210968687+therealagt@users.noreply.github.com>
2025-10-25 10:36:11 +0200

    db connection setup

---
+++ database/postgres.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+type Postgres struct {
+	DB *sql.DB
+	Host string 
+	Port int
+	User string 
+	Password string 
+	DBName string 
+}
+
+/* connect to db */
+func (p *Postgres) Connect() error {
+	connStr := fmt.Sprintf(
+    "host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+    p.Host, p.Port, p.User, p.Password, p.DBName,
+	)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Connect to PostgreSQL %v\n", err)
+		return err
+	}
+	p.DB = db
+	return nil
+}
+
+/* health check */ 
+func (p *Postgres) Ping() error {
+	if p.DB == nil {
+		return fmt.Errorf("no database connection")
+	}
+	return p.DB.Ping()
+}
